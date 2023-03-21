3cb2c5e7c0045c36baf8d75bfc4305b9be6736b4
julyagomes <julyamodena01@gmail.com>
2023-03-21 13:52:18 -0300

    acabando

---
+++ src/screens/Slider1/index.tsx
@@ -0,0 +1,9 @@
+import { FlatList, ImageBackground, View} from 'react-native';
+import { IPage } from '../../../App';
+import {
+    ComponentButtonSlider, ComponentListMarker, ComponentTitleSlider 
+} from '../../components';
+import { styles } from './styles';
+export function Slider1({setPageI}: IPage) {
+    const slide1 = require("")
+}
