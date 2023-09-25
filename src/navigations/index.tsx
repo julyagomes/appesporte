b4c6920e4c9235e33834d1b3b359b30b34a4f4c7
Alunos <julyagomes>
2023-09-25 10:25:32 -0300

    julyaaaa

--- src/navigations/index.tsx
+++ src/navigations/index.tsx
@@ -5,6 +5,7 @@ import {useAuth} from "../hooks/auth";
 import { TabNavigation } from './tab.navigation';
 export function Navigation() {
   const {user} = useAuth();
+  console.log(user)
   return (
     <NavigationContainer>
       {user?.token ? <TabNavigation /> : <LoginNavigation/>}
